bf907d5f0cbbbac15d0d454b3fc30dafd069bf8a
Barr-a-code-a <webdevnewsltr@gmail.com>
2022-04-03 21:13:38 +0200

    First Draft

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import AlbumItem from './Components/AlbumItem';
+
+function App() {
+  return (
+    <div className="App">
+    
+  
+
+     <AlbumItem />
+     <AlbumItem />
+     <AlbumItem />
+     <AlbumItem />
+     <AlbumItem />
+     <AlbumItem />
+     <AlbumItem />
+     <AlbumItem />
+     <AlbumItem />
+    
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/Components/AlbumItem.js
@@ -0,0 +1,12 @@
+
+function AlbumItem(){
+
+return <div>
+
+    <img className="Album" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" />
+</div>
+}
+
+
+
+export default AlbumItem;
