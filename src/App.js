5a2628dd595974bf303d0c7e3d45cae9b31a6cb9
Barr-a-code-a <webdevnewsltr@gmail.com>
2022-07-26 09:58:53 +0200

    <Mapped API images>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import './App.css';
-import AlbumItem from './Components/AlbumItem';
+import "./App.css";
+import AlbumItem from "./Components/AlbumItem";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [apiData, setApiData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const url = "https://jsonplaceholder.typicode.com/photos";
+      const res = await fetch(url);
+      const data = await res.json();
+      setApiData(data);
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-    
-  
-
-     <AlbumItem />
-     <AlbumItem />
-     <AlbumItem />
-     <AlbumItem />
-     <AlbumItem />
-     <AlbumItem />
-     <AlbumItem />
-     <AlbumItem />
-     <AlbumItem />
-    
+      {apiData.slice(20, 51).map((item) => (
+        <AlbumItem key={item.id} item={item} />
+      ))}
     </div>
   );
 }
 
 export default App;
-
--- src/Components/AlbumItem.js
+++ src/Components/AlbumItem.js
@@ -1,12 +1,33 @@
-
-function AlbumItem(){
-
-return <div>
-
-    <img className="Album" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" />
-</div>
-}
-
-
-
-export default AlbumItem;
+function AlbumItem(props) {
+    const { url, title } = props.item;
+    return (
+      <div className="col">
+        <div className="card shadow-sm">
+          <svg
+            className="bd-placeholder-img card-img-top"
+            width="100%"
+            height="225"
+            xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            aria-label="Placeholder: Thumbnail"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+          >
+            <rect width="100%" height="100%" fill="#55595c" />
+            <image
+              href={url}
+              height="225"
+              width="100%"
+              preserveAspectRatio="none"
+            />
+          </svg>
+  
+          <div className="card-body cardAreaText">
+            <p className="card-text">{title}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default AlbumItem;
